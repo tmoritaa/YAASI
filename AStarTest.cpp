692dfbea90ae68424ceb9f0aef3de09d61526d91
Tatsuya <tatsuya.ahad@gmail.com>
2011-09-03 14:29:42 -0400

    Modified a little bit more

--- AStarTest.cpp
+++ AStarTest.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <set>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
-int map[5][5] = {
+int graph[5][5] = 
+{
 	0, 0, 0, 0, 0,
 	0, 1, 1, 1, 0,
 	0, 0, 0, 1, 0,
@@ -12,13 +14,49 @@ int map[5][5] = {
 	0, 0, 0, 0, 0,
 };
 
-struct Node {	
+struct Node 
+{
+	friend bool operator<(const Node& n1, const Node& n2);
+	friend bool operator==(const Node& n1, const Node& n2);
 	int x;
 	int y;
 };
 
+bool operator<(const Node& n1, const Node& n2) 
+{
+	if (n1.x < n2.x) {
+		return true;
+	}
 
-bool AStar(Node start, Node goal) {
+	return false;
+}
+
+bool operator==(const Node& n1, const Node& n2) 
+{
+	if (n1.x == n2.x && n1.y == n2.y) {
+		return true;
+	}
+
+	return false;
+}
+
+int calc_heuristic(Node start, Node goal) 
+{
+	int dist = 0;
+	
+	int x = fabs(start.x - goal.x);
+	int y = fabs(start.y - goal.y);
+	
+	dist = floor(sqrt(x*x + y*y));
+
+	return dist;
+}
+
+bool AStar(Node start, Node goal) 
+{
+	Node x;
+	Node y;
+
 	// declarations
 	set<Node> closedSet;
 	set<Node> openSet; 	
@@ -31,7 +69,28 @@ bool AStar(Node start, Node goal) {
 	// initializations
 	openSet.insert(start);
 
-	g_score[start] = 
+	g_score[start] = 0;
+	h_score[start] = calc_heuristic(start, goal);
+	f_score[start] = h_score[start];
+
+	while (!openSet.empty()) {
+
+		set<Node>::iterator it = openSet.begin();
+		x = *it;
+
+		// check for lowest f_score among openSet
+		for (; it != openSet.end(); it++) {
+			if (f_score[*it] < f_score[x]) {
+				x = *it;	
+			}
+		}
+
+		if (x == goal) {
+			
+		}
+
+
+	}
 
 
 }
